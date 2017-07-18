8e9935ce9c71ecef63da215ea8cfa5a77497e276
Will Sun <will.sun@sap.com>
2017-07-18 15:47:00 +0800

    add some codes

--- src/app/man/man.component.spec.ts
+++ src/app/man/man.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ManComponent } from './man.component';
 
 describe('ManComponent', () => {
--- src/app/man/man.component.ts
+++ src/app/man/man.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JiraApi } from 'jira-client';
 
 @Component({
   selector: 'app-man',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./man.component.scss']
 })
 export class ManComponent implements OnInit {
+jira: JiraApi;
 
   constructor() { }
 
   ngOnInit() {
+    this.jira = new JiraApi({
+      protocol: 'https',
+      host: 'jira.hybris.com',
+      username: 'will.sun@sap.com',
+      password: 'Ihatepw20',
+      apiVersion: '2',
+      strictSSL: true
+    });
+
+    // test for get some data form jira server
+    this.jira.findIssue('CAAS-1492')
+      .then(issue => {
+        console.log(`Status: ${issue.fields.status.name}`);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+
+  }
+
+  testConnection() {
+    console.log('test connection');
   }
 
 }
