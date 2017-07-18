1ed09672bee1f081b895cdc90181eb0d7e870cd1
Will Sun <will.sun@sap.com>
2017-07-18 13:09:22 +0800

    finished project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialModule } from '../angular-material.component.module';
 import { AppComponent } from './app.component';
 import { ManComponent } from './man/man.component';
+import 'hammerjs'; // Required by Angular Material
 
 @NgModule({
   declarations: [
